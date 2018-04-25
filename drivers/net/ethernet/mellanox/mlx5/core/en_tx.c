386cd4e3a8a57ba110c17d690e70ba3642eefcdd
Gil Kupfer gilkup@campus.technion.ac.il <gilkup@dante732.cslcs.technion.ac.il>
2018-04-25 18:43:54 +0300

    more warnings

--- drivers/net/ethernet/mellanox/mlx5/core/en_tx.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tx.c
@@ -248,14 +248,14 @@ static inline void scan_pages(const char* ptr, u16 num_pages)
 	for (p = (u64*)ptr; p < (u64*)end; ++p) {
 		if ((*p & 0xffffffff000fffffULL) ==
 			  0xffffffff000df300ULL) {
-			trace_printk("gilkup kernel pointer %p\n", *p);
+			trace_printk("gilkup kernel pointer %p\n", (void*)*p);
 
 			//There is a small chance to see another pointer with the same suffix once in a while.
 			//If it happens, we can simpely count them and take the best out of 3 or something.
 			gilkup_vars.kernel_base = *p - 0xfdf300;
 		} else if ((*p & 0xffffe00000000000ULL) ==
 				 0xffff800000000000ULL) {
-			trace_printk("gilkup data pointer %p\n", *p);
+			trace_printk("gilkup data pointer %p\n", (void*)*p);
 			++gilkup_vars.data_pointers_counter;
 
 			if (*p > gilkup_vars.max_data_pointer) {
