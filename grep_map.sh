0c2478177deb87251e03e5072afb58b1793212d6
Markuze Alex <markuze@cs.technion.ac.il>
2018-05-11 20:12:10 +0300

    grep map

--- grep_map.sh
+++ grep_map.sh
@@ -1,3 +1,7 @@
 sudo grep -P "\Wstartup_64" /boot/System.map-4.14.0-kaslr
 sudo grep -P "\Wargv_split" /boot/System.map-4.14.0-kaslr
 sudo grep -P "\Wcall_usermodehelper$" /boot/System.map-4.14.0-kaslr
+
+sudo cat /proc/kallsyms | sudo grep -P "\Wstartup_64"
+sudo cat /proc/kallsyms |sudo grep -P "\Wargv_split"
+sudo cat /proc/kallsyms |sudo grep -P "\Wcall_usermodehelper$"
